2057749da5ef07e9ae9bcec64a988c2776880ee4
hassan1games <44328174+hassan1games@users.noreply.github.com>
2018-10-21 21:59:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "%";
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bcc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : **عدد الاعضاء المستلمين**`); 
- message.delete(); 
-};     
-});
 
 
 
@@ -21,16 +8,8 @@ client.on("message", message => {
 
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`%help`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
+
+
 
 
 
