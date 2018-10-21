6dbcb212548ff1eccfcd36fc7abd7e48f5bbcd2a
hassan1games <44328174+hassan1games@users.noreply.github.com>
2018-10-21 22:47:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,16 @@ var prefix = "%";
 
 
 
-
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`%help`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 
 
